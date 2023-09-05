b7890107305c5733dbb086e8201a55542ff0500c
Danny Krämer <d.kraemer@dotsource.de>
2023-09-05 07:43:56 +0200

    implement character sort

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,13 @@
-class Sorter {
-  collection: number[];
+import { CharactersCollection } from './CharactersCollection';
+import { NumbersCollection } from './NumbersCollection';
+import { Sorter } from './Sorter';
 
-  constructor(collection: number[]) {
-    this.collection = collection;
-  }
+const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+const charactersCollection = new CharactersCollection('Xaayb');
+const sorter = new Sorter(numbersCollection);
+const characterSorter = new Sorter(charactersCollection);
 
-  sort(): void {
-    const length = this.collection.length;
-
-    // Bubble Sort
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          const temp = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = temp;
-        }
-      }
-    }
-  }
-}
-
-const sorter = new Sorter([10, 3, -5, 0]);
+characterSorter.sort();
+console.log(charactersCollection.data);
 sorter.sort();
-console.log(sorter.collection);
+console.log(numbersCollection.data);
